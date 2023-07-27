c07dffcd793d278ea52707f889e191775365368e
Brian <brian@appsird.net>
2023-07-26 17:56:56 -0700

    remove prominent from buttons per guidelines

--- MultiMap/Buttons.swift
+++ MultiMap/Buttons.swift
@@ -24,47 +24,15 @@ struct Buttons: View {
             } label: {
                 Label("Playgrounds", systemImage: "figure.and.child.holdinghands")
             }
-            .buttonStyle(.borderedProminent)
+            .buttonStyle(.bordered)
             
             Button {
                 search(for: "beach")
             } label: {
                 Label("Beaches", systemImage: "beach.umbrella")
             }
-            .buttonStyle(.borderedProminent)
-            
-            Button {
-                let location = locationsHandler.manager.location
-                guard let latitude = location?.coordinate.latitude else { return }
-                guard let longitude = location?.coordinate.longitude else { return }
-
-                let central = MKCoordinateRegion(
-                    center: CLLocationCoordinate2D(latitude: latitude, longitude: longitude),
-                    span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-                    )
-
-                position = .region(central)
-            } label: {
-                Label("Central", systemImage: "plus.magnifyingglass")
-            }
             .buttonStyle(.bordered)
-            
-            Button {
-                let location = locationsHandler.manager.location
-                guard let latitude = location?.coordinate.latitude else { return }
-                guard let longitude = location?.coordinate.longitude else { return }
-
-                let wide = MKCoordinateRegion(
-                    center: CLLocationCoordinate2D(latitude: latitude, longitude: longitude),
-                    span: MKCoordinateSpan(latitudeDelta: 0.4, longitudeDelta: 0.4)
-                    )
-
-                position = .region(wide)
-            } label: {
-                Label("Wide", systemImage: "minus.magnifyingglass")
-            }
-            .buttonStyle(.bordered)
-            
+                        
         }
         .labelStyle(.iconOnly)
     }
